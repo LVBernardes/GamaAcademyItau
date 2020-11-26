938020892ad4e69ea56fe8e3080fbd28cbe377e4
liniplov <levb_28@hotmail.com>
2020-11-26 18:44:23 -0300

    adicionado classes de busca ao endpoint de agendamento

--- Gama06-Projeto/AgendaFacil/src/main/java/br/com/itau/AgendaFacil/dao/SchedulingDAO.java
+++ Gama06-Projeto/AgendaFacil/src/main/java/br/com/itau/AgendaFacil/dao/SchedulingDAO.java
@@ -1,10 +1,17 @@
 package br.com.itau.AgendaFacil.dao;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.itau.AgendaFacil.model.Scheduling;
 
 public interface SchedulingDAO extends CrudRepository<Scheduling, Integer> {
 	
+	public ArrayList<Scheduling> findAllByOrderByAgency();
+	public ArrayList<Scheduling> findAllByAgency(int agencyID);
+	public ArrayList<Scheduling> findAllBySchedulingDate(LocalDate date);
+	public ArrayList<Scheduling> findAllByCustomerNameContains(String name);
 
 }
