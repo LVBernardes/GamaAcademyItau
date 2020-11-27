6133e3c9a13ba2c134e1124f7169b583c52e4e3d
liniplov <levb_28@hotmail.com>
2020-11-27 11:20:47 -0300

    ajuste de regras não-funcionais da pagina de relatorio

--- Gama06-Projeto/Front_AgendaFacil/reports.js
+++ Gama06-Projeto/Front_AgendaFacil/reports.js
@@ -32,7 +32,7 @@ function loadAgencies(tagID) {
 
 
 function createSelectorOptions(list, listID, listName, tagID) {
-    let selectorHTML = `<option selected>Selecione uma opção</option>`;
+    let selectorHTML = `<option value="0"  selected>Selecione uma opção</option>`;
 
     for (let i = 0; i < list.length; i++) {
         selectorHTML += `<option value="${list[i].id}">${list[i].name}</option>`;
@@ -49,52 +49,143 @@ function generateReport() {
     let agencyCheck = + document.getElementById("agencyCheck").checked;
     let dateCheck = + document.getElementById("dateCheck").checked;
     let nameCheck = + document.getElementById("nameCheck").checked;
+    let agencyFilter = document.getElementById("agencyFilter");
+    let dateFilter = document.getElementById("dateFilter");
+    let nameFilter = document.getElementById("nameFilter");
+
+    let dateType = 1;
 
     //console.log(agencyCheck + " " + dateCheck + " " + nameCheck);
     let mode = parseInt("" + nameCheck + dateCheck + agencyCheck, 2);
 
     console.log(mode);
+
     switch (mode) {
         case 0:
-            agencyFilter = 0;
-            dateFilter = "1/1/01";
-            nameFilter = "";
+            agencyFilterValue = 0;
+            dateFilterValue = "01/01/01";
+            nameFilterValue = "";
             break;
         case 1:
-            agencyFilter = document.getElementById("agencyFilter").value;
-            dateFilter = "1/1/01";
-            nameFilter = "";
+            agencyFilterValue = agencyFilter.value;
+            dateFilterValue = "01/01/01";
+            nameFilterValue = "";
+            if (agencyFilterValue == 0) {
+                agencyFilter.focus();
+                alert("Selecione uma agência antes de prosseguir!")
+                return 1;
+            }
             break;
         case 2:
-            agencyFilter = 0;
-            console.log(document.getElementById("dateFilter").value);
-            dateFilter = convertISODateToUSADate(document.getElementById("dateFilter").value);
-            nameFilter = "";
+            agencyFilterValue = 0;
+            dateFilterValue = convertISODateToCustomDate(dateFilter.value, dateType);
+            nameFilterValue = "";
+            if (!dateFilterValue) {
+                dateFilter.focus();
+                alert("Selecione uma data antes de prosseguir!");
+                return 1;
+            }
             break;
         case 3:
-            agencyFilter = document.getElementById("agencyFilter").value;
-            dateFilter = convertISODateToUSADate(document.getElementById("dateFilter").value);
-            nameFilter = "";
+            agencyFilterValue = agencyFilter.value;
+            dateFilterValue = convertISODateToCustomDate(dateFilter.value, dateType);
+            nameFilterValue = "";
+            if (agencyFilterValue == 0) {
+                if (dateFilterValue == "") {
+                    agencyFilter.focus();
+                    alert("Selecione a agência e a data antes de prosseguir!");
+                    return 1;
+                } else {
+                    agencyFilter.focus();
+                    alert("Selecione a agência antes de prosseguir!");
+                    return 1;
+                }
+            }
             break;
         case 4:
-            agencyFilter = 0;
-            dateFilter = "1/1/01";
-            nameFilter = document.getElementById("nameFilter").value;
+            agencyFilterValue = 0;
+            dateFilterValue = "01/01/01";
+            nameFilterValue = nameFilter.value;
+            if (nameFilterValue == "") {
+                nameFilter.focus();
+                alert("Preencha um nome antes de prosseguir!");
+                return 1;
+            }
             break;
         case 5:
-            agencyFilter = document.getElementById("agencyFilter").value;
-            dateFilter = "1/1/01";
-            nameFilter = document.getElementById("nameFilter").value;
+            agencyFilterValue = agencyFilter.value;
+            dateFilterValue = "01/01/01";
+            nameFilterValue = nameFilter.value;
+            if (agencyFilterValue == 0) {
+                if (nameFilterValue == "") {
+                    agencyFilter.focus();
+                    alert("Selecione a agência e preencha um nome antes de prosseguir!");
+                    return 1;
+                } else {
+                    agencyFilter.focus();
+                    alert("Selecione a agência antes de prosseguir!");
+                    return 1;
+                }
+            }
             break;
         case 6:
-            agencyFilter = 0;
-            dateFilter = convertISODateToUSADate(document.getElementById("dateFilter").value);
-            nameFilter = document.getElementById("nameFilter").value;
+            agencyFilterValue = 0;
+            dateFilterValue = convertISODateToCustomDate(dateFilter.value, dateType);
+            nameFilterValue = nameFilter.value;
+            if (dateFilterValue == "") {
+                if (nameFilterValue == "") {
+                    dateFilter.focus();
+                    alert("Selecione uma data e preencha um nome antes de prosseguir!");
+                    return 1;
+                } else {
+                    dateFilter.focus();
+                    alert("Selecione uma data antes de prosseguir!");
+                    return 1;
+                }
+            }
             break;
         case 7:
-            agencyFilter = document.getElementById("agencyFilter").value;
-            dateFilter = convertISODateToUSADate(document.getElementById("dateFilter").value);
-            nameFilter = document.getElementById("nameFilter").value;
+            agencyFilterValue = agencyFilter.value;
+            dateFilterValue = convertISODateToCustomDate(dateFilter.value, dateType);
+            nameFilterValue = nameFilter.value;
+
+            if (agencyFilterValue == 0) {
+                if (dateFilterValue == "") {
+                    if (nameFilterValue == "") {
+                        agencyFilter.focus();
+                        alert("Selecione a agência, a data e preencha um nome antes de prosseguir!");
+                        return 1;
+                    } else {
+                        agencyFilter.focus();
+                        alert("Selecione a agência e a data antes de prosseguir!");
+                        return 1;
+                    }
+
+                } else {
+                    if (nameFilterValue == "") {
+                        agencyFilter.focus();
+                        alert("Selecione a agência e preencha um nome antes de prosseguir!");
+                        return 1;
+                    } else {
+                        agencyFilter.focus();
+                        alert("Selecione a agência antes de prosseguir!");
+                        return 1;
+                    }
+
+                }
+            } else {
+                if (dateFilterValue == "") {
+                    if (nameFilterValue == "") {
+                        dateFilter.focus();
+                        alert("Selecione uma data e preencha um nome antes de prosseguir!");
+                        return 1;
+                    } else {
+                        dateFilter.focus();
+                        alert("Selecione uma data antes de prosseguir!");
+                        return 1;
+                    }
+                }
+            }
             break;
 
     }
@@ -102,9 +193,9 @@ function generateReport() {
     let url = new URL(backendURL + apiPath)
     let params = {
         mode: mode,
-        name: nameFilter,
-        date: dateFilter,
-        agencyID: agencyFilter
+        name: nameFilterValue,
+        date: dateFilterValue,
+        agencyID: agencyFilterValue
     }
     Object.keys(params).forEach(key => url.searchParams.append(key,
         params[key]));
@@ -127,17 +218,66 @@ function showSchedulings(response) {
     if (response.status == 200) {
         console.log(response.status);
         console.log(response.data);
+
+        let listHTML = presentReportHTML(response.data);
+
+        document.getElementById("reportPanel").innerHTML = listHTML;
+
     } else {
         console.log(response.status);
         console.log(response);
     }
 }
 
-function convertISODateToUSADate(dateISO) {
-    date = dateISO.split("-");
-    let year = date[0].slice(2);
-    let month = date[1];
-    let dt = date[2];
+function convertISODateToCustomDate(dateISO, type) {
+    if (dateISO != "") {
+        date = dateISO.split("-");
+        let year = date[0].slice(2);
+        let month = date[1];
+        let dt = date[2];
 
-    return (month + '/' + dt + '/' + year);
+        switch (type) {
+            case 0:
+                return (month + '/' + dt + '/' + year);
+            case 1:
+                return (dt + '/' + month + '/' + year);
+        }
+
+    } else {
+        return "";
+    }
+
+}
+
+function presentReportHTML(array) {
+
+    reportLines = "";
+
+    for (let i = 0; i < array.length; i++) {
+        reportLines += `<div class="row reportLine">
+        <div class="col-xs-6 col-sm-6 col-md-2 col-lg-1 col-xl-2">
+           ${array[i].schedulingDate}
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-2 col-lg-1 col-xl-1">
+           ${array[i].schedulingTime}
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-4 col-xl-4">
+           ${array[i].customerName}
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+           ${array[i].customerEmail}
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-2">
+           ${array[i].customerPhone}
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+           Agencia: ${array[i].agency.name}
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+           ${array[i].observation}
+        </div>
+        </div>`;
+    }
+    return reportLines;
+
 }
