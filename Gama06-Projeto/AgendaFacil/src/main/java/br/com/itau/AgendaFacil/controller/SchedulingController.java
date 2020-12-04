4d7a62f044f474941efd884d11bda770c21f44e8
Liniplov <levb_28@hotmail.com>
2020-12-03 21:52:37 -0300

    ajustando dependencia do Maven para java 11

--- Gama06-Projeto/AgendaFacil/src/main/java/br/com/itau/AgendaFacil/controller/SchedulingController.java
+++ Gama06-Projeto/AgendaFacil/src/main/java/br/com/itau/AgendaFacil/controller/SchedulingController.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
